b12e1e4826ab488f1c79e6978ade2da18291c57e
faizankhanchang22-gif <faizankhanchang22@gmail.com>
2025-10-31 10:19:49 +0500

    Implement GET route for downloading files

--- Api downloadroute.ts
+++ Api downloadroute.ts
@@ -1,1 +1,10 @@
+import { NextRequest } from 'next/server'
 
+export async function GET(req: NextRequest) {
+  const url = req.nextUrl.searchParams.get('url')
+  if (!url) return Response.json({error: 'URL daal bhai!'})
+
+  const res = await fetch(`https://advanceytdownloader.ytansh038.workers.dev/?url=${url}`)
+  const data = await res.json()
+  return Response.json(data)
+}
